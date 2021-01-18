b7ea6c71ba446bada4fb19c60f44b9efbe9512f1
thallesyasmim <thallesgabriel1307@gmail.com>
2021-01-18 20:59:03 -0300

    Create context in JavaScript

--- 01/src/index.js
+++ 01/src/index.js
@@ -1,11 +1,21 @@
-const EntityBase = require('./entityBase')
+// const EntityBase = require('./entityBase')
 
-console.log(new EntityBase({
-    name: 'Yasmim Cristina',
-    gender: 'female'
-}).name)
+// console.log(new EntityBase({
+//     name: 'Yasmim Cristina',
+//     gender: 'female'
+// }).name)
 
-console.log(new EntityBase({
-    name: 'Thalles Gabriel',
-    gender: 'male'
-}).name)
+// console.log(new EntityBase({
+//     name: 'Thalles Gabriel',
+//     gender: 'male'
+// }).name)
+
+const assert = require('assert')
+const Employee = require('./employee')
+
+{ // Diz que estamos criando um contexto, as variáveis que serão criadas não poderão ser acessadas de fora
+    const yasmim = 'Linda'
+}
+{ // Por exemplo, não dará erro
+    const yasmim = 'Linda'
+}
