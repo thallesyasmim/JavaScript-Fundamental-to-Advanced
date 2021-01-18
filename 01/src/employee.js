9b9f7f74e613b66c148a822d247d99d120d17199
thallesyasmim <thallesgabriel1307@gmail.com>
2021-01-18 20:53:53 -0300

    Static Prop && grossPay getter

--- 01/src/employee.js
+++ 01/src/employee.js
@@ -1,7 +1,15 @@
 const EntityBase = require("./entityBase")
 
 class Employee extends EntityBase { // Herança
-    
+    static #TAXES_PERCENTUAL = 0.2 // Privado mesmo que seja um membro estático
+    #grossPay = 5000.40
+
+    get grossPay() {
+        return Intl.NumberFormat('pt-br', {
+            currency: 'BRL',
+            style: 'currency'
+        }).format(this.#grossPay)
+    }
 }
 
 module.exports = Employee
