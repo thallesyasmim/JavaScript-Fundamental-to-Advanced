ab004b2186ce20487585de202b9b2914e2fa6b7f
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-01-21 12:50:41 -0300

    unFormatCurrency Util

--- 01/src/util.js
+++ 01/src/util.js
@@ -7,6 +7,12 @@ class Util { // Também chamado de 'Helped'
     static formatCurrency(value) {
         return this.#defaultFormat.format(value)
     }
+
+    // R$ 1000,10 - 1000.10
+    static unFormatCurrency(value) {
+        return Number(value.replace(/\D/g, '')) / 100 // Deve tirar tudo o que é ponto, espaço, 'R$', mas ainda respeitando a vírgula. 
+        // Tudo que for digíto e não for digíto será tirado
+    }
 }
 
 module.exports = Util
