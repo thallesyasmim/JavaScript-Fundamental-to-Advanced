d97700c917ab7c7c3d59e8df336807030d0e56c6
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-01-21 12:56:20 -0300

    netPay Polimorfismo

--- 01/src/manager.js
+++ 01/src/manager.js
@@ -9,7 +9,9 @@ class Manager extends Employee {
         return Util.formatCurrency(this.#bonuses)
     } 
 
-    
+    get netPay() { // Transformação - netPay + bonuses
+        return Util.formatCurrency(Util.unFormatCurrency(super.netPay) + Util.unFormatCurrency(this.bonuses)) // super - Para acessar atributos, getters, setters da classe pai (Se utilizassemos o this, nos retornaria o método que usamos com polimorfismo)
+    }
 }
 
 module.exports = Manager
