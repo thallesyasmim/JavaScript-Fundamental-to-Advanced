ad212854beb362b4251a31a042aac612a4d35f3d
thallesyasmim <thallesgabriel1307@gmail.com>
2021-01-25 12:21:33 -0300

    Declare Const's

--- 02/contadorComponent.js
+++ 02/contadorComponent.js
@@ -1,9 +1,17 @@
+const BTN_REINICIAR = 'btnReiniciar'
+const ID_CONTADOR = 'contador'
+const VALOR_CONTADOR = 100
+const PERIODO_INTERVALO = 10
+
 class ContadorComponent {
     constructor() {
         this.inicializar()
     }
 
     inicializar() {
-        console.log('Inicializou!');
+        console.log('Inicializou!')
+
+
+
     }
 }
