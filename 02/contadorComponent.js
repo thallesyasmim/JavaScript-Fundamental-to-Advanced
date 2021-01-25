d78f96249c43165be29df6b5296a1e4018cec1e1
thallesyasmim <thallesgabriel1307@gmail.com>
2021-01-25 12:39:23 -0300

    Proxy Object

--- 02/contadorComponent.js
+++ 02/contadorComponent.js
@@ -8,8 +8,26 @@ class ContadorComponent {
         this.inicializar()
     }
 
+    preparContadorProxy() {
+        const handler = {
+            set: (currentContext, propertyKey, newValue) => { // Método acessório SET para o objeto Proxy - Executado sempre que o objeto passado no construtor do Proxy mudar
+                console.log({ currentContext, propertyKey, newValue })
+                console.log('Contexto Atual - Objeto que está sendo passado no construtor do Proxy', currentContext)
+
+                currentContext[propertyKey] = newValue
+                return true
+            }
+        }
+        
+        const contador = new Proxy({ // Proxy - Observar a instância, com o objeto que está sendo passado no seu construtor. Quando esse objeto mudar, ele vai executar uma função.
+            valor: VALOR_CONTADOR,
+            efetuarParada: () => {}
+        }, handler)
+    }
+
     inicializar() {
         console.log('Inicializou!')
+        const elementoContador = document.getElementById(ID_CONTADOR) // OUTPUT
 
 
 
