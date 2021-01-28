3e0322f008a1b569746d6801c9ed704d2ac608ca
thallesyasmim <thallesgabriel1307@gmail.com>
2021-01-27 21:43:47 -0300

    describe()

--- 03/test/todo.test.js
+++ 03/test/todo.test.js
@@ -2,5 +2,5 @@ const { describe, it, before } = require('mocha') // Mocha - nosso motor de rend
 const { expect } = require('chai') // Chai - para fazer asserções, vimos o módulo Assert do Node.js também, porém vamos usar o Chai por ser algo mais facilmente visível
 
 describe('todo', () => {
-    
+
 })
