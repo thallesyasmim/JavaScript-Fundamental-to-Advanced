a2dd92b13c65143a7d86d59cf7892c9107a2ea57
thallesyasmim <thallesgabriel1307@gmail.com>
2021-01-27 21:27:03 -0300

    Todo List Test

---
+++ 03/test/todo.test.js
@@ -0,0 +1,6 @@
+const { describe, it, before } = require('mocha') // Mocha - nosso motor de renderização, não precisamos usar o Node em sí aqui
+const { expect } = require('chai') // Chai - para fazer asserções, vimos o módulo Assert do Node.js também, porém vamos usar o Chai por ser algo mais facilmente visível
+
+describe('todo', () => {
+    
+})
