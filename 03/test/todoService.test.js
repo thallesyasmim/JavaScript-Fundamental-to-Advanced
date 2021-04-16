af3570a7d8671cf3c1ebe397675f4b7137ac3709
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-04-16 18:06:22 -0300

    switch #list

--- 03/test/todoService.test.js
+++ 03/test/todoService.test.js
@@ -1,12 +1,41 @@
-const { describe, it, before, afterEach } = require('mocha')
+const { describe, it, before, beforeEach } = require('mocha')
 const { expect } = require('chai')
-const TodoRepository = require('../src/todoRepository')
+const TodoService = require('../src/todoService')
 const { createSandbox } = require('sinon')
 
 describe('todoService', () => {
-	describe('', () => {
-		it('')
-		it('')
-		it('')
+	let sandBox
+	before(() => {
+		sandBox = createSandbox()
+	})
+	describe('#list', () => {
+		const mockDatabase = [
+			{
+				name: 'Yasmim',
+				age: 17,
+				meta: { revision: 0, created: 1618598872772, version: 0 },
+				'$loki': 1
+			}
+		]
+
+		let todoService
+		beforeEach(() => {
+			const dependencies = {
+				todoRepository: {
+					list: sandBox.stub().returns(mockDatabase)
+				}
+			}
+
+			todoService = new TodoService(dependencies)
+		})
+
+		it('should return data on a specific format', () => {
+			const result = todoService.list()
+			const [{ meta, $loki, ...expected }] = mockDatabase
+			expect(result).to.be.deep.equal([expected])
+		})
 	})
 })
+
+/* Partimos na suposição que nosso todoRepository está funcionando (até porque não queremos testá-lo novamente), testando
+o todoService */
