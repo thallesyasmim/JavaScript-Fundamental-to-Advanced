855822d103fff8ccad9d934570bc040fb44cdc8a
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-04-16 14:08:35 -0300

    eslint

--- 03/src/todo.js
+++ 03/src/todo.js
@@ -1,19 +1,19 @@
 const uuid = require('uuid') // Uma das nossas dependências já instalou essa LIB
 
 class Todo {
-    constructor({ text, when }) {
-        this.text = text
-        this.when = when
+	constructor({ text, when }) {
+		this.text = text
+		this.when = when
 
-        this.status = ''
-        this.id = uuid.v4()
-    }
+		this.status = ''
+		this.id = uuid.v4()
+	}
 
-    isValid() {
-        // 0 - '' - null - false = false
-        return !!this.text && !isNaN(this.when.valueOf()) // !! - Transforma o valor em booleano mantendo se ele é true ou false
-        // valueOf() - Retorna o valor com typeof number, no caso da data retorna em milisegundos | isNan() - Verifica se não é um número
-    }
+	isValid() {
+		// 0 - '' - null - false = false
+		return !!this.text && !isNaN(this.when.valueOf()) // !! - Transforma o valor em booleano mantendo se ele é true ou false
+		// valueOf() - Retorna o valor com typeof number, no caso da data retorna em milisegundos | isNan() - Verifica se não é um número
+	}
 }
 
 module.exports = Todo
--- 03/src/todoRepository.js
+++ 03/src/todoRepository.js
@@ -1,18 +1,18 @@
 const loki = require('lokijs') // Não vamos utilizar PostgreSQL ou mySQL, mas sim um banco de dados de memória. O lokijs entre nesse contexto para coisas mais simples (não em produção)
 
 class TodoRepository {
-    constructor() {
-        const db = new loki('todo', {}) // Instanciando nosso banco de dados
-        this.schedule = db.addCollection('schedule') // Adicionando uma nova tabela "schedule"
-    }
+	constructor() {
+		const db = new loki('todo', {}) // Instanciando nosso banco de dados
+		this.schedule = db.addCollection('schedule') // Adicionando uma nova tabela "schedule"
+	}
 
-    list() { // Listagem das tarefas
-        return this.schedule.find()
-    }
+	list() { // Listagem das tarefas
+		return this.schedule.find()
+	}
 
-    create(data) { // Criação de novos dados 
-        return this.schedule.insertOne(data)
-    }
+	create(data) { // Criação de novos dados 
+		return this.schedule.insertOne(data)
+	}
 }
 
 module.exports = TodoRepository
