7c9d24f2a99b7f41424f90dc1e264ec8cf81e584
thallesyasmim <thallesgabriel1307@gmail.com>
2021-04-17 13:09:05 -0300

    todoService test

--- 03/test/todoService.test.js
+++ 03/test/todoService.test.js
@@ -1,6 +1,7 @@
-const { describe, it, before, beforeEach } = require('mocha')
+const { describe, it, before, beforeEach, afterEach } = require('mocha')
 const { expect } = require('chai')
 const TodoService = require('../src/todoService')
+const Todo = require('../src/todo')
 const { createSandbox } = require('sinon')
 
 describe('todoService', () => {
@@ -35,7 +36,91 @@ describe('todoService', () => {
 			expect(result).to.be.deep.equal([expected])
 		})
 	})
+
+	describe('#create', () => {
+		let todoService
+		beforeEach(() => {
+			const dependencies = {
+				todoRepository: {
+					create: sandBox.stub().returns(true)
+				}
+			}
+
+			todoService = new TodoService(dependencies)
+		})
+
+		afterEach(() => sandBox.restore())
+
+		it('shouldn\'t save todo item with invalid data', () => {
+			const data = new Todo({
+				text: '',
+				when: ''
+			})
+
+			// delete data.id | Horrível para performance
+			Reflect.deleteProperty(data, 'id') // Reflect - Substituir objetos respeitando o clico de vida do JS, sem prejudicar a performance
+			const expected = {
+				error: {
+					message: 'Invalid data',
+					data
+				}
+			}
+
+			const result = todoService.create(data)
+			expect(result).to.be.deep.equal(expected)
+
+		})
+		it('should save todo item with late status when the property is further then today', () => {
+			const properties = {
+				text: 'I must walk my dog',
+				when: new Date('2020-12-01 12:00:00 GMT-0') // Conclusão da tarefa
+			}
+	
+			const expectedId = '00001'
+			const uuid = require('uuid')
+			const fakeUUID = sandBox.fake.returns(expectedId) 
+			sandBox.replace(uuid, 'v4', fakeUUID) // V4 é uma função nomeada, quando for chamada retornará um dado fake
+			
+			const data = new Todo(properties)
+			const today = new Date('2020-12-02')
+			sandBox.useFakeTimers(today.getTime()) // Não vai dar problema no nosso teste, não importa quanto tempo passe
+
+			todoService.create(data)
+
+			const expectedCallWith = {
+				...data,
+				status: 'late'
+			}
+
+			expect(todoService.todoRepository.create.calledOnceWithExactly(expectedCallWith)).to.be.ok
+		})
+
+		it('should save todo item with pending status', () => {
+			const properties = {
+				text: 'I must walk my dog',
+				when: new Date('2020-12-10 12:00:00 GMT-0') // Conclusão da tarefa
+			}
+	
+			const expectedId = '00001'
+			const uuid = require('uuid')
+			const fakeUUID = sandBox.fake.returns(expectedId) 
+			sandBox.replace(uuid, 'v4', fakeUUID) // V4 é uma função nomeada, quando for chamada retornará um dado fake
+			
+			const data = new Todo(properties)
+			const today = new Date('2020-12-02')
+			sandBox.useFakeTimers(today.getTime()) // Não vai dar problema no nosso teste, não importa quanto tempo passe
+
+			todoService.create(data)
+
+			const expectedCallWith = {
+				...data,
+				status: 'pending'
+			}
+
+			expect(todoService.todoRepository.create.calledOnceWithExactly(expectedCallWith)).to.be.ok
+		})
+	})
 })
 
-/* Partimos na suposição que nosso todoRepository está funcionando (até porque não queremos testá-lo novamente), testando
+/* Partimos da suposição que nosso todoRepository está funcionando (até porque não queremos testá-lo novamente), testando
 o todoService */
